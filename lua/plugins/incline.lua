910a9a75bd0fc580206466bfd46ead7a61b3f0ba
P217F <vuongiavinhdieu2k9@gmail.com>
2025-12-05 12:50:11 +0700

    #48: Edit incline.nvim and increase treesitter size of files

--- lua/plugins/incline.lua
+++ lua/plugins/incline.lua
@@ -1,54 +1,47 @@
 return {
-  "b0o/incline.nvim",
-  event = "VeryLazy",
-  config = function()
-    local colors = {
-      bg_active = "#3c3836",
-      fg_active = "#ebdbb2",
-      bg_inactive = "#282828",
-      fg_inactive = "#a89984",
-    }
-
-    require("incline").setup({
-      debounce_threshold = { falling = 50, rising = 10 },
-      hide = { cursorline = false, focused_win = false, only_win = false },
-      highlight = {
-        groups = {
-          InclineNormal = { default = true, guibg = colors.bg_active, guifg = colors.fg_active, gui = "bold" },
-          InclineNormalNC = { default = true, guibg = colors.bg_inactive, guifg = colors.fg_inactive },
-        },
-      },
-      ignore = {
-        buftypes = "special",
-        filetypes = {},
-        floating_wins = true,
-        unlisted_buffers = true,
-        wintypes = "special",
+  {
+    "b0o/incline.nvim",
+    dependencies = {
+      "SmiteshP/nvim-navic",
+    },
+    opts = {
+      window = {
+        padding = 0,
+        margin = { horizontal = 0, vertical = 0 },
       },
       render = function(props)
-        local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
-        if filename == "" then filename = "[No Name]" end
-        if vim.bo[props.buf].modified then filename = "[+] " .. filename end
+        local helpers = require 'incline.helpers'
+        local navic = require 'nvim-navic'
+        local devicons = require 'nvim-web-devicons'
 
-        local icon, icon_color = require("nvim-web-devicons").get_icon_color(filename)
-        if not icon then icon = "" icon_color = colors.fg_active end
+        local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ':t')
+        if filename == '' then
+          filename = '[No Name]'
+        end
 
-        return { { " " .. icon, guifg = icon_color }, { " " }, { filename .. " " } }
+        local ft_icon, ft_color = devicons.get_icon_color(filename)
+        local modified = vim.bo[props.buf].modified
+
+        local res = {
+          ft_icon and { ' ', ft_icon, ' ', guibg = ft_color, guifg = helpers.contrast_color(ft_color) } or '',
+          ' ',
+          { filename, gui = modified and 'bold,italic' or 'bold' },
+          guibg = '#3c3836',
+        }
+
+        if props.focused then
+          for _, item in ipairs(navic.get_data(props.buf) or {}) do
+            table.insert(res, {
+              { ' > ', group = 'NavicSeparator' },
+              { item.icon, group = 'NavicIcons' .. item.type },
+              { item.name, group = 'NavicText' },
+            })
+          end
+        end
+
+        table.insert(res, ' ')
+        return res
       end,
-      window = {
-        margin = { horizontal = 1, vertical = 1 },
-        options = { signcolumn = "no", wrap = false },
-        overlap = { borders = true, statusline = false, tabline = false, winbar = false },
-        padding = 0,
-        padding_char = " ",
-        placement = { horizontal = "right", vertical = "top" },
-        width = "fit",
-        winhighlight = {
-          active = { EndOfBuffer = "None", Normal = "InclineNormal", Search = "None" },
-          inactive = { EndOfBuffer = "None", Normal = "InclineNormalNC", Search = "None" },
-        },
-        zindex = 50,
-      },
-    })
-  end,
+    },
+  },
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,4 +1,3 @@
-
 return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
@@ -17,7 +16,7 @@ return {
         enable = true,
         additional_vim_regex_highlighting = false,
         disable = function(lang, buf)
-          local max_filesize = 2048*2048
+          local max_filesize = 8182*8182
           local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
           if ok and stats and stats.size > max_filesize then
             return true

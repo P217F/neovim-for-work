eaee295d86415cb89f2583f89ac228003e9de5a7
P217F <vuongiavinhdieu2k9@gmail.com>
2025-12-06 08:45:41 +0700

    #50: Add neogit plugins support for gitsigns

---
+++ lua/plugins/neogit.lua
@@ -0,0 +1,25 @@
+return {
+    "NeogitOrg/neogit",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "sindrets/diffview.nvim",
+    },
+    cmd = "Neogit",
+    keys = {
+        { "<leader>g", "<cmd>Neogit<CR>" },
+    },
+    config = function()
+        require("neogit").setup({
+            disable_commit_confirmation = true,
+            kind = "tab",
+            integrations = {
+                diffview = true,
+            },
+            signs = {
+                section = { "", "" },
+                item = { "", "" },
+                hunk = { "", "" },
+            },
+        })
+    end,
+}
